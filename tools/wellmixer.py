41d68c92027073dd565bf53354cf1ef58cfa5aee
ChrillePille <christian@weigelt.se>
2022-06-01 13:14:06 +0200

    add export to csv, add some comments

--- tools/wellmixer.py
+++ tools/wellmixer.py
@@ -6,6 +6,7 @@ from numpy import random as rng
 import networkx as nx
 from tqdm import tqdm
 
+# check if OK to put id in well w
 def put_ok(G, w, id):
     ids = [sub for sub in w]#[sub[0] for sub in w]
     if all(id not in G[i] for i in ids) and (id not in ids):
@@ -32,24 +33,16 @@ def add_to_subs(subs, well):
         subs[-1] = s
     return subs
 
-
-def mixer(inp=False, n=15000, m=5, k=5):
+def mixer(n=15000, m=5, k=5):
+    # set spin threshold
     threshold = 100
-    if not inp:
-        print(f"n: {n}\nm: {m}\nk: {k}")
-    else:
-        try:
-            n = int(input("# of substances n: "))
-            m = int(input("# of substances/well m: "))
-            k = int(input("# of occurences/substance k: "))
-        except:
-            print("Please input an integer. Exitting")
-            return
     t0 = time.time()
     subs = np.empty(n, dtype=object)
+    #init ids, occs left
     for id in range(n):
         subs[id] = [id, k]#[id, f"{id}", k] # serial, id, occs left
 
+    # init return list and nieghbor graph
     wells = []
     G = nx.Graph()
     G.add_nodes_from([sub[0] for sub in subs])
@@ -59,10 +52,12 @@ def mixer(inp=False, n=15000, m=5, k=5):
     spins = 0
     th_hit = 0
     reshuffles = 0
+    # run until empty
     while subs.size > 0:
         w = []
         del_list = []
         loops = 0
+        # randomly select substances and check if they can be put in current well
         for m_i in range(min(m, len(subs))):
             subcount += 1
             found = False # get sub
@@ -78,7 +73,7 @@ def mixer(inp=False, n=15000, m=5, k=5):
                     continue
                 index = rng.choice(np.arange(subs.size)) # choose index
                 loops += 1
-                s = subs[index] # get triple
+                s = subs[index] # get tuple
                 id = s[0]
                 num = s[1]
                 if put_ok(G, w, s[0]) and (s[1] > 0): # check if chosen tup has no edges to subs already "in well"
@@ -133,8 +128,37 @@ def mixer(inp=False, n=15000, m=5, k=5):
     print(f"iterations={subcount}, spins={spins}, hitrate={hitrate:.4%}")
     print(f"avg. spins per well: {spins/len(wells)}")
     print(f"threshhold hit # times: {th_hit}\n# of reshuffles: {reshuffles}")
-    stats = {'dims':{n, m, k}, 'time':p2, 'ef':hitrate}
+    stats = {'dims':[n, m, k], 'time':p2, 'ef':hitrate}
     return wells, stats
+
+
+def i_to_coord(index):
+    return f"{chr(ord('A') + (index // 22))}{str((index % 22) + 1).zfill(2)}"
+
+def to_csv(num, wells, map):
+    tl = map
+    if map == None:
+        tl = str
+    ret = []
+    for w_i in range(len(wells)):
+        w = wells[w_i]
+        w_s = ",".join([tl(i) for i in w])
+        ret.append(f"{num},{i_to_coord(w_i)},{w_s}\n")
+    return ret
+
+#returns csv string of 
+def divider(wells, id_map):
+    out = []
+    plate = 1
+    while len(wells) > 0:
+        buf = wells[:352]
+        if len(wells) > 352:
+            wells = wells[352:]
+        else:
+            wells = []
+        out.extend(to_csv(plate, buf, id_map))
+        plate += 1
+    return out
 
 n = 15000
 m = 5
@@ -142,23 +166,44 @@ k = 5
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', action="store_true", dest='p', default=False)
-parser.add_argument('-i', action="store_true", dest='i', default=False)
 parser.add_argument('-s', action="store_true", dest='s', default=False)
 parser.add_argument('-c', action="store", dest="c", type=int, default=1)
 parser.add_argument('-n', action="store", dest="n", type=int, default=n)
 parser.add_argument('-m', action="store", dest="m", type=int, default=m)
 parser.add_argument('-k', action="store", dest="k", type=int, default=k)
+parser.add_argument('-o', action="store_true", dest='o', default=False)
+parser.add_argument('-file', action="store", type=str, default=None)
 
 res = parser.parse_args()
-if res.p:
-    print("Run with profiler\n")
-    for i in range(res.c):
-        cProfile.run(f'mixer(inp={res.i})')
+if res.file != None:
+    with open(res.file, 'r') as f:
+        #read and tidy up batch id file
+        batch_ids = f.readlines()
+        batch_ids = (' '.join([batch_ids[i].strip() for i in range(len(batch_ids))])).split()
+        b_map = {i:batch_ids[i].strip() for i in range(len(batch_ids))}
+
+        #mix
+        wells, stats = mixer(n=len(batch_ids), m=res.m, k=res.k)
+        print(stats)
+
+        #to csv
+        out = divider(wells, lambda x: b_map[x])
+        print("Printing to csv file: out.csv")
+        with open("out.csv", 'w') as f:
+            f.writelines(out)
+elif res.o:
+    print("Export to csv")
+    wells, stats = mixer(n=res.n, m=res.m, k=res.k)
+    print(stats)
+    out = divider(wells, filename=res.file)
+    print("Printing to csv file: out.csv")
+    with open("out.csv", 'w') as f:
+        f.writelines(out)
 else:
     print("Run without profiler:\n")
     data = []
     for i in range(res.c):
-        wells, stats = mixer(inp=res.i, n=res.n, m=res.m, k=res.k)
+        wells, stats = mixer(n=res.n, m=res.m, k=res.k)
         data.append(stats)
     if res.s:
         # save data
