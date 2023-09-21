1801befb17c5632f4d877760536c90a7f034fc4b
Mats Dahlberg <dahlbma@gmail.com>
2023-09-21 09:32:50 +0200

    Delete logfiles older that 3 days

--- frontend/main.py
+++ frontend/main.py
@@ -50,6 +50,31 @@ logger.addHandler(ch)
 logger.addHandler(fh)
 
 
+####################################################################
+### Delete old log-files
+directory_path = os.path.dirname(os.path.realpath(__file__))
+
+# Get the current date and time
+current_datetime = datetime.datetime.now()
+# Calculate the time threshold (3 days ago)
+threshold_datetime = current_datetime - datetime.timedelta(days=3)
+# List all files in the directory
+file_list = os.listdir(directory_path)
+
+# Iterate through the files and delete .log files older than 3 days
+for file_name in file_list:
+    file_path = os.path.join(directory_path, file_name)
+    
+    # Check if the file has a .log extension and if it's older than 3 days
+    if file_name.endswith(".log") and datetime.datetime.fromtimestamp(os.path.getctime(file_path)) < threshold_datetime:
+        try:
+            logging.getLogger().info(f"Deleting: {file_name}")
+            os.remove(file_path)
+        except Exception as e:
+            print(f"Error deleting {file_name}: {str(e)}")
+###
+####################################################################
+
 v_path = os.path.join(".", "ver.dat")
 version = ""
 if os.path.exists(v_path):
